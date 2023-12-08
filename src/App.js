fd98f61d24838d3ac4d9e2f320d572652164c00c
aakash <aakashramesh018@gmail.com>
2023-12-09 05:14:15 +0530

    added the swap component

--- src/App.js
+++ src/App.js
@@ -4,12 +4,12 @@ import Portfolio from './Components/Portfolio';
 import Transactions from './Components/Transactions';
 import Followers from './Components/Followers';
 import Following from './Components/Following';
+import Swap from './Components/Swap';
 
 function App() {
   return (
     <div>
-      <Followers />
-      <Following />
+      <Swap />
     </div>
   );
 }
